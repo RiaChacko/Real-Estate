02cfb2e0de8c189915e3afb0a48465b85a4fc787
Ria Chacko <chacko.ria2003@gmail.com>
2023-12-30 19:12:51 -0500

    Sign up backend functionality

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 import express from "express";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 const app = express();
+app.use(express.json());
 mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log("Connected to DB");
     
@@ -19,6 +21,7 @@ app.listen(3000, ()=>{
 })
 
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
 
 
 
